2931ef58d1678ff003c2b29a183c71109a724463
Fernando Soares <fernando.hfs94@gmail.com>
2020-06-06 14:37:34 -0300

    :sparkles: Add project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import { syncHistoryWithStore } from 'mobx-react-router';
+import { createBrowserHistory } from 'history';
+import * as Sentry from '@sentry/browser';
+
+import Routes from './routes';
+import * as store from './mobx';
+import { router } from './mobx';
+
+import './plugins/sentry.plugin';
+import 'semantic-ui-css/semantic.min.css';
+
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
+
+const rootElement = document.getElementById('root');
+const browserHistory = createBrowserHistory();
+
+const history = syncHistoryWithStore(browserHistory, router);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider {...store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
